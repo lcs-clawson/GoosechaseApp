4079059fe3295c590d853ac4422ce0f811df7a65
Cooper Lawson <CSLawson@lcs.on.ca>
2022-11-16 11:35:47 -0500

    Initial Commit

---
+++ GoosechaseApp/GoosechaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoosechaseAppApp.swift
+//  GoosechaseApp
+//
+//  Created by Cooper Lawson on 2022-11-16.
+//
+
+import SwiftUI
+
+@main
+struct GoosechaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
