d5f5f261f21bfca2effd733f08b4a8bf0fa1f6b7
Cooper Lawson <CSLawson@lcs.on.ca>
2022-11-18 09:44:13 -0500

    It's going ight

--- GoosechaseApp/ContentView.swift
+++ GoosechaseApp/ContentView.swift
@@ -7,41 +7,51 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-       
+        
         
         ZStack {
             
             Color.white
             
             VStack {
-                    
-                Text("Experiences")
-                    .font(.title .weight(.black))
+                
+                Text ("Experiences")
+                    .font(.title.weight(.black))
                 
                 HStack {
-                   
-                    Image("Winter Division")
+                    Image ("Winter Division")
                         .resizable()
-                        .scaledToFit()
+                        .scaledToFill()
+                        .frame(width: 50, height: 50, alignment: .center)
+                        
                     
                     VStack {
                         Text("Winter Wonderland Section B")
                             .font(.title2)
                             .bold()
+                        
                         Text("Final team building activity of module 5!")
                         
                     }
+                    
+                    }
+                HStack {
+                    Image ("Goose Feet")
+                        .resizable()
+                        .scaledToFill()
+                }
+                    
                 }
             }
-            .padding()
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+
