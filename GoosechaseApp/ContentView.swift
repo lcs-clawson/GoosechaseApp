41cddc8a33a92ae44265752782b8710c49e440c0
Cooper Lawson <CSLawson@lcs.on.ca>
2022-11-18 09:58:32 -0500

    got de three boxes but don't have frames

--- GoosechaseApp/ContentView.swift
+++ GoosechaseApp/ContentView.swift
@@ -39,9 +39,34 @@ struct ContentView: View {
                     
                     }
                 HStack {
-                    Image ("Goose Feet")
+                  Image ("Goose Toes")
                         .resizable()
                         .scaledToFill()
+                        .frame(width: 50, height: 55, alignment: .center)
+                    
+                    VStack {
+                        Text("Grade 9 Orientation Intersession...")
+                            .font(.title2)
+                            .bold()
+                        
+                        Text("All submissions must follow covid guidlines (masked when inside, masked or 2m distanced when outside.) All submissions must be taken outside, unless otherwise noted!")
+                    }
+                }
+                
+                HStack {
+                    Image("Toilet Paper")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 50, height: 50, alignment: .center)
+                    
+                    VStack {
+                        Text("Integrated Scavenger Hunt Janu...")
+                            .font(.title2)
+                            .bold()
+                        
+                        Text("Get to know your teammates, get outside and be active, and get pumped to WIN BIG!!")
+                        
+                    }
                 }
                     
                 }
